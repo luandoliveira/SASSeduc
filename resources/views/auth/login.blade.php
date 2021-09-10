c5c540a7f30d6fdde15fca7cf3e8c5d6f7e17250
luandoliveira <luanf.d.silva@gmail.com>
2021-09-10 10:30:13 -0400

    Logo da SAES

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
     <div class="container-fluid h-custom">
       <div class="row d-flex justify-content-center ">
         <div id="ajuste1" class="col-md-9 col-lg-6 col-xl-5">
-          <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-login-form/draw2.png" class="img-fluid"
+          <img src="img/logo.png" class="img-fluid"
            >
         </div>
         <div  id="ajuste2" class="col-md-10 col-lg-6 col-xl-4 offset-xl-1">
